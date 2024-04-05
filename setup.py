5996534ab1c3f4661acae93bbf63ca6b36d675b6
skords <skords@gmail.com>
2024-04-05 20:28:37 +0530

    fix: encoding

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Avantis Labs",
     author_email="admin@avantisfi.com",
     description="SDK for interacting with Avantis trading contracts.",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://avantisfi.com/",
     packages=find_packages(),
