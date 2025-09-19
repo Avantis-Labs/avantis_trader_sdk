2bc1ceef325ed21ab9a2d2c6ddb464f0bcdc562e
Yug Kapoor <skords@gmail.com>
2025-09-19 21:50:38 +0530

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="avantis_trader_sdk",
-    version="0.8.3",
+    version="0.8.4",
     author="Avantis Labs",
     author_email="brank@avantisfi.com",
     description="SDK for interacting with Avantis trading contracts.",
